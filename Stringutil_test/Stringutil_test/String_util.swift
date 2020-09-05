e9dd45ae3105d249c835ebed5067f6efebc40e72
ImDaeseong <cs93059@gmail.com>
2020-09-05 17:38:40 +0900

    DaeseongSwift

---
+++ Stringutil_test/Stringutil_test/String_util.swift
@@ -0,0 +1,248 @@
+
+import Foundation
+
+public func getExt(sInput : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    if let lastindex = sInput.lastIndex(of: ".") {
+        
+        let sResult : String =  String(sInput[ sInput.index( after: lastindex) ..< sInput.endIndex])
+        
+        return sResult
+    }
+    
+    return nil
+}
+ 
+public func getName(sInput : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    if let lastindex = sInput.lastIndex(of: "/") {
+        let sResult = String( sInput[ sInput.index( after: lastindex) ..< sInput.endIndex] )
+        return sResult
+    }
+    
+    return nil
+}
+
+public func getUpper(sInput : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    return sInput.uppercased()
+}
+
+public func getLower(sInput : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    return sInput.lowercased()
+}
+
+public func getSplitFirst(sInput : String, sType : Character) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    let sResult = sInput.split(separator: sType)
+    return String(sResult.first!)
+}
+
+public func getSplitFirstA(sInput : String, sType : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    let sResult = sInput.components(separatedBy: sType)
+    if let first = sResult.first {
+        return first
+    }
+    return nil
+}
+
+public func getSplitLast(sInput : String, sType : Character) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    let sResult = sInput.split(separator: sType)
+    
+    return String(sResult.last!)
+}
+
+public func getSplitLastA(sInput : String, sType : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    let sResult = sInput.components(separatedBy: sType)
+    if let last = sResult.last {
+        return last
+    }
+    return nil
+}
+
+public func getSplit(sInput : String, sType : Character) -> [String]? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    return sInput.split(separator : sType).map(String.init)
+}
+
+public func getSubString(sInput : String, sStart : String, sEnd : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    var sResult : String = sInput
+    
+    if let index = sResult.range(of: sStart) {
+        
+        sResult.removeSubrange(sResult.startIndex ..< index.upperBound)
+    }
+    
+    if let index = sResult.range(of: sEnd) {
+        
+        sResult.removeSubrange(index.lowerBound ..< sResult.endIndex)
+    }
+    
+    return sResult
+}
+
+public func getLeft(sInput : String, length : Int) ->String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    if length <= sInput.count {
+        
+        /*
+        //첫번째 방법
+        let nStart = sInput.startIndex
+        let nEnd = sInput.index(sInput.endIndex, offsetBy: length - sInput.count)
+        let sResult = sInput[nStart ..< nEnd]
+        return String(sResult)
+        */
+        
+        /*
+        //두번째 방법
+        let index = sInput.index(sInput.startIndex, offsetBy: length)
+        let sResult = sInput[..<index]
+        return String(sResult)
+        */
+        
+        //세번째 방법
+        let sResult = sInput.prefix(length)
+        return String(sResult)
+    }
+    return nil
+}
+
+public func getRight(sInput : String, length : Int) ->String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    if length <= sInput.count {
+        
+        /*
+        //첫번째 방법
+        let nStart = sInput.index(sInput.startIndex, offsetBy: sInput.count - length)
+        let nEnd = sInput.endIndex
+        let sResult = sInput[nStart ..< nEnd]
+        return String(sResult)
+        */
+       
+        /*
+        //두번째 방법
+        let index = sInput.index(sInput.endIndex, offsetBy: -(length))
+        let sResult = sInput[index...]
+        return String(sResult)
+        */
+        
+        //세번째 방법
+        let sResult = sInput.suffix(length)
+        return String(sResult)
+    }
+    return nil
+}
+
+public func getReplace(sInput : String, sFind : String, sReplace : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    let sResult = sInput.replacingOccurrences(of: sFind, with: sReplace)
+    return sResult
+}
+
+public func getTrim(sInput : String) -> String? {
+    
+    if sInput == "" {
+        return nil
+    }
+    
+    let sResult = sInput.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    
+    return sResult
+}
+
+public func getFileSize(size : Double) -> String? {
+    
+    var sResult : String?
+    
+    if size < 1024 {
+        
+        sResult = String(format: "%d", arguments: [Int(size)])
+        
+    } else if size < (1024 * 1024) {
+        
+        let float = (size/1024)
+        sResult = String(format: "%.1fKB", arguments: [float])
+        
+    } else if size < (1024 * 1024 * 1024) {
+        
+        let float = (size/(1024*1024))
+        sResult = String(format: "%.1fMB", arguments: [float])
+        
+    } else {
+    
+        let float = (size/(1024*1024*1024))
+        sResult = String(format: "%.1fGB", arguments: [float])
+    }
+    
+    return sResult
+}
+
+public func getTime() ->String {
+    
+    let format1 = "yyyy-MM-dd hh:mm:ss"
+    //let format2 = "yyyy-MM-dd"
+    //let format3 = "yyyy년 MM월 dd일 HH시 mm분 ss초"
+    
+    let dataFormat = DateFormatter()
+    dataFormat.locale = Locale(identifier: "ko")
+    dataFormat.dateFormat = format1
+    return dataFormat.string(from: Date())
+}
+
---
+++ Stringutil_test/Stringutil_test/ViewController.swift
@@ -0,0 +1,57 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func btn1_click(_ sender: UIButton) {
+        
+        
+        let sResult1 = "C:/Program Files/Internet Explorer/iexplore.exe"
+        print("getExt: \(getExt(sInput: sResult1)!)")
+        print("getName: \(getName(sInput: sResult1)!)")
+        print("getUpper: \(getUpper(sInput: sResult1)!)")
+        print("getLower: \(getLower(sInput: sResult1)!)")
+        print("getSubString: \(getSubString(sInput: sResult1, sStart: ":/", sEnd: "/")!)")
+        print("getLeft: \(getLeft(sInput: sResult1, length: 10)!)")
+        print("getRight: \(getRight(sInput: sResult1, length: 12)!)")
+        
+        
+        let sResult2 : String = "사과,감자,파인애플,귤,포도,당근,고구마"
+        print("getSplitFirst: \(getSplitFirst(sInput: sResult2, sType: ",")!)")
+        print("getSplitFirstA: \(getSplitFirstA(sInput: sResult2, sType: ",")!)")
+        print("getSplitLast: \(getSplitLast(sInput: sResult2, sType: ",")!)")
+        print("getSplitLastA: \(getSplitLastA(sInput: sResult2, sType: ",")!)")
+        print("getReplace: \(getReplace(sInput : sResult2, sFind : ",", sReplace : "|")!)")
+        
+        let splits = getSplit(sInput: sResult2, sType: ",")!
+        for item in splits {
+            print("getSplit: \(item)")
+        }
+        
+        
+        let sResult3 : String = "     앞에공백제거  마지막공백제거      "
+        print("getTrim:\(getTrim(sInput : sResult3)!)")
+    
+        
+        
+        let length1 : Double = 1023
+        let length2 : Double = (1024 * 1000)
+        let length3 : Double = (1024 * 1024 * 345)
+        let length4 : Double = (1024 * 1024 * 1024 * 245)
+        print("getFileSize: \(getFileSize(size : length1)!)")
+        print("getFileSize: \(getFileSize(size : length2)!)")
+        print("getFileSize: \(getFileSize(size : length3)!)")
+        print("getFileSize: \(getFileSize(size : length4)!)")
+        
+        
+        print("getTime: \(getTime())")
+        
+        
+    }
+    
+}
+
