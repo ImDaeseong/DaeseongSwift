31921719a9106f49c7ac72d1f3f7190b5ac4e7b6
ImDaeseong <cs93059@gmail.com>
2020-09-25 18:31:46 +0900

    DaeseongSwift

---
+++ UIStackView_test/UIStackView_test/UIStackViewEx2.swift
@@ -0,0 +1,52 @@
+import UIKit
+import Foundation
+
+class UIStackViewEx2 : UIStackView {
+    
+    let view1: UIView = {
+        let view1 = UIView()
+        view1.backgroundColor = UIColor.red
+        view1.layer.cornerRadius = 10
+        view1.clipsToBounds = true
+        return view1
+    }()
+    
+    let view2: UIView = {
+        let view2 = UIView()
+        view2.backgroundColor = UIColor.green
+        view2.layer.cornerRadius = 10
+        view2.clipsToBounds = true
+        return view2
+    }()
+    
+    let view3: UIView = {
+        let view3 = UIView()
+        view3.backgroundColor = UIColor.blue
+        view3.layer.cornerRadius = 10
+        view3.clipsToBounds = true
+        return view3
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initControl()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        initControl()
+    }
+    
+    func initControl(){
+        
+        self.axis = .vertical
+        self.distribution = .fillEqually
+        self.alignment = .fill
+        self.spacing = 5
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addArrangedSubview(view1)
+        self.addArrangedSubview(view2)
+        self.addArrangedSubview(view3)
+    }
+}
